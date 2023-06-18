a36697523b719814d67cc92e95addba2e1c366b0
noklash <123834419+noklash@users.noreply.github.com>
2023-06-18 09:30:07 +0300

    installed svgr using npm

---
+++ my-project/src/Landing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Logo from "./assets/lo"
+
+export default function Landing(){
+    return (
+        <div>
+            <nav>{Logo}</nav>
+            <img/>
+            <div>
+                <h3></h3>
+                <p></p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <button></button>
+            </div>
+            
+        </div>
+    )
+}
