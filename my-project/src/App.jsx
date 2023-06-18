96136e08822a45d5a571aa9c70e6dc224c2769e4
noklash <123834419+noklash@users.noreply.github.com>
2023-06-18 12:33:30 +0300

    moved the image files

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Landing from './Landing'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
+  
     <>
-      <h1 className="text-3xl font-bold underline">
+      {/* <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+    </h1> */}
+      <Landing/>
     </>
   )
 }
